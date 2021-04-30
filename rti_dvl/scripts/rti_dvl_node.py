96a5aea875457c38c1518c48e7378e2909acbbdb
jake <john.mcconnell.1874@gmail.com>
2021-04-30 11:17:44 -0400

    Added commenting to DVL node

--- rti_dvl/scripts/rti_dvl_node.py
+++ rti_dvl/scripts/rti_dvl_node.py
@@ -7,6 +7,7 @@ from rti_dvl.msg import DVL, Command, BottomTrack
 import pynmea2
 import serial
 
+#define a dictionary of salinities
 SALINITY = {'fresh': 0, 'salt': 35}
 
 
@@ -49,30 +50,37 @@ class SyncTime(object):
 
 
 def write_to_dvl(s, duration=0.5):
+    '''A simple function to write a command down to the DVL
+    '''
     dvl.write((s.strip() + '\r'))
-    
     rospy.sleep(duration)
     
 
-
 if __name__ == '__main__':
+
+    #init the node
     rospy.init_node('rti_dvl_node')
 
+    #get the port for the DVL
     dev = rospy.get_param('~dev', '/dev/ttyUSB0')
+
     # Get configurations split by ;
     config = rospy.get_param('~commands', '').split(';')
-    #print config
 
+    #if there is no rosparm for water set it to fresh
     if not rospy.has_param('/water'):
         rospy.set_param('/water', 'fresh')
     water = rospy.get_param('/water', 'fresh')
     salinity = SALINITY[water]
 
+    #define the ros topics this node publishes to
     dvl_pub = rospy.Publisher('/rti/body_velocity/raw', DVL, queue_size=100)
     bt_pub = rospy.Publisher('/rti/bottom_tracking/raw', BottomTrack, queue_size=100)
 
+
     while not rospy.is_shutdown():
         try:
+            #open the serial device
             rospy.loginfo('Open device {}'.format(dev))
             #dvl = serial.Serial(dev, baudrate = 115200, dsrdtr=True, rtscts=True, timeout=1.0)
             dvl = serial.Serial(dev,baudrate = 115200,timeout = 1.0)
@@ -85,80 +93,102 @@ if __name__ == '__main__':
     if rospy.is_shutdown():
         sys.exit(-1)
 
-    # STOP
+    #Stop the DVL. This is done to write the desired config. The dvl does not accept any
+    #commands while pinging other than 'STOP'
+    #Note this is reqired, we set salinity and zero the pressure sensor
     while dvl.in_waiting:
-        line = dvl.readline()
-	print line
 
+        #read from the DVL
+        line = dvl.readline()
+
+        #check if the DVL is already stopped
         if line.strip().startswith('STOP'):
             break
-        write_to_dvl('STOP')
-	#write_to_dvl('STOP')
-	#write_to_dvl('STOP')
 
+        #issue the command to stop and log it at the ros level
+        write_to_dvl('STOP')
         rospy.loginfo_throttle(1.0, 'Trying to stop DVL pinging...')
 
     # Zero pressure sensor
     write_to_dvl('CPZ')
-    # Change water salinity
+
+    # Change water salinity to the desired config
     write_to_dvl('CWS {}'.format(salinity))
-    # Write extra commands in config file
+
+    # Write extra commands in config file, this is done from the launch file
+    #be extremly carful with what you tell the DVL
+    #the reccomended way of reconfiguring the DVL is via the pulse windows application from Rowe
     for c in config:
         if c:
             write_to_dvl(c)
             rospy.loginfo('Write to DVL: ' + c)
 
+    #currently unsure what this is supposed to accomplish
     cmd = Command()
-
     line = dvl.readline()
-
     cp = line.strip().split(' ', 1)
-    print cp
 
     while True:
+        
+        #ask the dvl to send up it's current configuration, log at the ros level
         write_to_dvl('CSHOW')
         rospy.loginfo_throttle(1.0, 'Trying to read DVL commands...')
 
-
+        #flag to break from the loop, did we recive the DVL config
         received = False
         while dvl.in_waiting:
-	    print "test"
+
+            #read from the dvl
             line = dvl.readline()
 
+            #decode the message and check the current config
             cp = line.strip().split(' ', 1)
             if len(cp) == 2:
                 c, p = cp
-		print c
                 if c == 'CEPO':
                     received = True
                 c = c.split('[', 1)[0]
                 if c in cmd.__slots__:
                     setattr(cmd, c, p)
         
+        #if we have it, break from this loop, otherwise wait 1 second and try again
         if received:
             break
-        else:
+        else: 
             rospy.sleep(1.0)
 
-    # Start
+    #Tell the DVL to start up, time to get to work, log at ros level
     while not dvl.in_waiting:
         write_to_dvl('START')
         rospy.loginfo_throttle(1.0, 'Trying to start DVL pinging...')
 
+    #object for time sync
     sync = SyncTime()
+
+    #object to decode the DVL data
     reader = pynmea2.NMEAStreamReader(errors='ignore')
 
-    bt = None
+    #log at the ros level we have started the DVL
+    rospy.loginfo_throttle(1.0, 'DVL Started, verify rostopic')
+
+    #main loop to run the DVL driver
+    bt = None #consider remove of bottom track stuff, presently does nothing
     while not rospy.is_shutdown():
+
+        #read from the serial device
         try:
             char = dvl.read()
         except serial.SerialException:
             break
-        for msg in reader.next(char):
+        for msg in reader.next(char): # decode the message
+            #check if the message is a Rowe DVL message sent of RS232 or RS485
             if isinstance(msg, pynmea2.types.rti.RTI01) or isinstance(msg, pynmea2.types.rti.RTI03):
+                
+                #record the time of message
                 stamp = sync.sync(msg.time / 100.0, rospy.Time.now())
 
-                # For back compability
+                # For back compability Note from jake: very confused by what Jinkun means here
+                #populate the DVL message with the info from the decoded serial device
                 d = DVL()
                 d.header.stamp = stamp
                 d.velocity.x = msg.x / 1000.0
@@ -169,6 +199,7 @@ if __name__ == '__main__':
                 d.time = msg.time / 100.0
                 dvl_pub.publish(d)
 
+                #old style message, never used
                 bt = BottomTrack()
                 bt.header.stamp = stamp
                 bt.command = cmd
@@ -180,6 +211,7 @@ if __name__ == '__main__':
                 bt.time = msg.time / 100.0
                 bt.sample = msg.number
 
+            #check if it is an IMU? No idea what is up here, but please leave this code
             if isinstance(msg, pynmea2.types.rti.RTI30) or isinstance(msg, pynmea2.types.rti.RTI32):
                 if bt is None:
                     continue
@@ -188,4 +220,5 @@ if __name__ == '__main__':
                 bt.orientation.z = msg.heading
                 bt_pub.publish(bt)
 
+    #close the serial object on shutdown
     dvl.close()
